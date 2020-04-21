47dc0d3bb74d34e4dc8d030679142d8e46dbb966
Axel Örn Sigurðsson <axel@absalon.is>
2020-04-21 14:24:16 +0100

    Add new base "models" for dict and member entries

--- dict_typer/models.py
+++ dict_typer/models.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Set
+from typing import Any, Dict, List, Optional, Set, Union
 
 from dict_typer.utils import is_valid_key
 
@@ -128,6 +128,103 @@ class TypedDefinion:
         assert isinstance(other, self.__class__)
 
         return set(self.members) == set(other.members)
+
+
+class MemberEntry:
+    """ A representation of a type with optional sub types
+
+    A MemberEntry without subtypes can be considered as a leaf of a tree, in
+    most cases it will be a simple unit such as str, int, float, but it can
+    also be a List, Set, Tuple, Dict without any known members.
+    """
+
+    name: str
+    sub_members: Set[Union["MemberEntry", "DictEntry"]]
+
+    def __init__(
+        self,
+        name: str,
+        sub_members: Optional[Set[Union["MemberEntry", "DictEntry"]]] = None,
+    ) -> None:
+        self.name = name
+        self.sub_members = sub_members or set()
+
+    def __hash__(self) -> int:
+        return hash(str(self))
+
+    def __repr__(self) -> str:
+        return f"<MemberEntry ({self})>"
+
+    def __str__(self) -> str:
+        def get_member_value(item: Union["MemberEntry", "DictEntry"]) -> str:
+            """ Only reference DictEntry by name """
+            if isinstance(item, DictEntry):
+                return item.name
+            return str(item)
+
+        if len(self.sub_members) == 2 and "None" in (
+            sm.name for sm in self.sub_members
+        ):
+            optional_member = next(sm for sm in self.sub_members if sm.name != "None")
+            return f"{self.name}[Optional[{get_member_value(optional_member)}]]"
+        if len(self.sub_members) > 0:
+            sub_members_strs = sorted(get_member_value(sm) for sm in self.sub_members)
+            if len(sub_members_strs) == 1:
+                return f"{self.name}[{sub_members_strs[0]}]"
+            return f"{self.name}[Union[{', '.join(sub_members_strs)}]]"
+
+        return self.name
+
+
+class DictEntry:
+    """ A representation of a typed dict
+
+    A typed dict will have a name and a members map. The value of each member
+    is a MemberEntry, which has a name and an optional submembers.
+    """
+
+    name: str
+    members: Dict[str, MemberEntry]
+    indentation: int
+    force_alternative: bool
+
+    def __init__(
+        self,
+        name: str,
+        members: Optional[Dict[str, MemberEntry]],
+        indentation: int = 4,
+        force_alternative: bool = False,
+    ) -> None:
+        self.name = name
+        self.members = members or {}
+        self.indentation = indentation
+        self.force_alternative = force_alternative
+
+    def __repr__(self) -> str:
+        return f"<DictEntry ({self})>"
+
+    def __str__(self) -> str:
+        out: List[str] = []
+
+        if self.force_alternative:
+            out.append(f'{self.name} = TypedDict("{self.name}", {{')
+
+            for key, value in self.members.items():
+                if isinstance(value, DictEntry):
+                    out.append(f'{" " * self.indentation}"{key}": {value.name}')
+                else:
+                    out.append(f'{" " * self.indentation}"{key}": {value}')
+
+            out.append("})")
+        else:
+            out.append(f"class {self.name}(TypedDict):")
+            for key, value in self.members.items():
+                if isinstance(value, DictEntry):
+                    out.append(f"{' ' * self.indentation}{key}: {value.name}")
+                else:
+                    out.append(f"{' ' * self.indentation}{key}: {value}")
+
+        return "\n".join(out)
 
 
 class NestedDictRef:
---
+++ tests/unit/test_models.py
@@ -0,0 +1,91 @@
+from dict_typer.models import DictEntry, MemberEntry
+
+
+def test_member_entry_base_output() -> None:
+    entry = MemberEntry("str")
+
+    assert str(entry) == "str"
+
+
+def test_member_entry_sub_types() -> None:
+    entry = MemberEntry("List", sub_members={MemberEntry("str")})
+
+    assert str(entry) == "List[str]"
+
+
+def test_member_entry_sub_types_union() -> None:
+    entry = MemberEntry("List", sub_members={MemberEntry("str"), MemberEntry("int")})
+
+    assert str(entry) == "List[Union[int, str]]"
+
+
+def test_member_entry_sub_types_optional_if_just_one_type() -> None:
+    entry = MemberEntry("List", sub_members={MemberEntry("str"), MemberEntry("None")})
+
+    assert str(entry) == "List[Optional[str]]"
+
+
+def test_member_entry_sub_types_union_instead_of_optional_if_multiple_types() -> None:
+    entry = MemberEntry(
+        "List",
+        sub_members={MemberEntry("str"), MemberEntry("int"), MemberEntry("None")},
+    )
+
+    assert str(entry) == "List[Union[None, int, str]]"
+
+
+def test_member_entry_is_hashable_based_on_str_out() -> None:
+    assert hash(MemberEntry("int")) == hash(MemberEntry("int"))
+
+    hashed_list_int_1 = hash(MemberEntry("List", sub_members={MemberEntry("int")}))
+    hashed_list_int_2 = hash(MemberEntry("List", sub_members={MemberEntry("int")}))
+    hashed_list_str = hash(MemberEntry("List", sub_members={MemberEntry("str")}))
+
+    assert hashed_list_int_1 == hashed_list_int_2
+    assert hashed_list_int_1 != hashed_list_str
+
+
+def test_dict_entry_base_output() -> None:
+    entry = DictEntry(
+        "RootType", members={"foo": MemberEntry("str"), "bar": MemberEntry("int")}
+    )
+
+    # fmt: off
+    assert str(entry) == "\n".join([
+        "class RootType(TypedDict):",
+        "    foo: str",
+        "    bar: int",
+    ])
+    # fmt: on
+
+
+def test_dict_entry_alternative_output() -> None:
+    entry = DictEntry(
+        "RootType",
+        members={"foo": MemberEntry("str"), "bar": MemberEntry("int")},
+        force_alternative=True,
+    )
+
+    # fmt: off
+    assert str(entry) == "\n".join([
+        'RootType = TypedDict("RootType", {',
+        '    "foo": str',
+        '    "bar": int',
+        '})',
+    ])
+    # fmt: on
+
+
+def test_member_entry_with_dict_entry() -> None:
+    dict_entry = DictEntry("SubType", members={"foo": MemberEntry("str")})
+    entry = MemberEntry("List", sub_members={dict_entry})
+
+    assert str(entry) == "List[SubType]"
+
+
+def test_dict_entry_with_member_entry() -> None:
+    dict_entry = DictEntry("SubType", members={"foo": MemberEntry("str")})
+    member_entry = MemberEntry("List", sub_members={dict_entry})
+    entry = MemberEntry("Set", sub_members={member_entry})
+
+    assert str(entry) == "Set[List[SubType]]"
