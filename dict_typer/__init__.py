3a1620fbb8778b516b362b6b025335ad668fa471
Axel Örn Sigurðsson <axel@absalon.is>
2020-05-05 15:57:07 +0100

    Add optional rich output with the rich library

--- dict_typer/__init__.py
+++ dict_typer/__init__.py
@@ -4,6 +4,8 @@ import sys
 from typing import Dict, Tuple
 
 import click
+from rich.console import Console
+from rich.syntax import Syntax
 
 from dict_typer.type_definitions import get_type_definitions
 
@@ -16,9 +18,18 @@ __version__ = "0.1.8"
     default=True,
     help="Show imports at the top, default: True",
 )
+@click.option("--rich", "-r", is_flag=True, help="Show rich output.")
+@click.option(
+    "--line-numbers", "-l", is_flag=True, help="Show line numbers if rich.",
+)
 @click.argument("file", type=click.File("r"), nargs=-1)
 @click.version_option(__version__)
-def cli(file: Tuple[io.TextIOWrapper], imports: bool = True) -> None:
+def cli(
+    file: Tuple[io.TextIOWrapper],
+    imports: bool = True,
+    rich: bool = False,
+    line_numbers: bool = False,
+) -> None:
     if len(file) > 1:
         raise click.BadArgumentUsage("Multiple files supplied, run with one at a time")
     if len(file) == 0:
@@ -37,5 +48,9 @@ def cli(file: Tuple[io.TextIOWrapper], imports: bool = True) -> None:
         raise click.UsageError(f"JSON serialisation error \n\n{e}")
 
     output = get_type_definitions(parsed, show_imports=imports)
-
-    click.echo(output)
+    if rich:
+        syntax = Syntax(output, "python", theme="monokai", line_numbers=line_numbers)
+        console = Console()
+        console.print(syntax)
+    else:
+        click.echo(output)
